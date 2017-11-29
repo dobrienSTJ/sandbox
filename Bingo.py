9c64d8a8d2bb44939cfbee0de6502bdc40590afa
Daniel O'Brien <15dobrien@stjosephsrush.com>
2017-11-29 10:44:42 +0000

    Update Bingo.py

--- Bingo.py
+++ Bingo.py
@@ -1,27 +1,28 @@
 from time import sleep
 import random
 
+
 mynumber=random.randint(0,50)
 
 Daniel = []
 
 Daniel.append(mynumber)
+	
+
 
 game = 0
 
 while(game < 6):
   
-  number=random.randint(0,50)
-  number=(int(number))
-  numberstr = (str(number))
+	number=random.randint(0,50)
+	number=(int(number))
+	numberstr = (str(number))
+
+
+	print(Daniel)
+	print(numberstr)
+	sleep(1)
   
-  for i in numberstr:
-  
-  print(Daniel)
-  print(i)
-  sleep(1)
-  
-  if Daniel == number:
-    print("Bingo!")
-    game = game + 1
-    
+	if number in Daniel:
+		print("Bingo!")
+		game = game + 1
